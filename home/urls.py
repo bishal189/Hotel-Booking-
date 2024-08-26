acb51229f83945b134e0443c3868c72135463a12
bishal89 <bishalmurmu150@gmail.com>
2024-08-26 15:22:42 +0545

    updated book now btn

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     
     path('', views.home,name='home'),
+    path('all-rooms/', views.all_rooms,name='all_rooms'),
 ]
--- home/views.py
+++ home/views.py
@@ -14,3 +14,11 @@ def home(request):
         'hotel_room':hotel_room,
     }
     return render(request,'home/home.html',context)
+
+
+def all_rooms(request):
+    hotel_room=HotelRoom.objects.all().order_by('-id')  
+    context={
+        'hotel_room':hotel_room
+    } 
+    return render(request,'home/listing.html',context)
