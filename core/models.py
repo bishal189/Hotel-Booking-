fbbc744ada25d9131a5a6acba26ffe7aace57e76
bishal89 <bishalmurmu150@gmail.com>
2024-08-26 22:17:09 +0545

    added model for payment and api

--- core/models.py
+++ core/models.py
@@ -75,3 +75,24 @@ class Booking(models.Model):
    
     def __str__(self):
         return f'Booking for {self.guest_name} in Room {self.room.room_number}'
+
+
+
+
+class Payment(models.Model):
+    PAYMENT_METHODS = (
+        ('credit_card', 'Credit Card'),
+        ('paypal', 'PayPal'),
+        ('bank_transfer', 'Bank Transfer'),
+      
+    )
+
+    
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=20, choices=PAYMENT_METHODS)
+    payment_status = models.CharField(max_length=20, default='Pending') 
+    payment_date = models.DateTimeField(auto_now=True,null=True, blank=True)
+    transaction_id = models.CharField(max_length=100, blank=True, null=True) 
+
+    def __str__(self):
+        return f'Payment of {self.amount} via {self.payment_method} - Status: {self.payment_status}'
